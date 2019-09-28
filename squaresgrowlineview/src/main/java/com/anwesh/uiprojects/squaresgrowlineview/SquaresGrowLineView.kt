c1a2db923b4003ec6f05351ba0773b52bb15b0fc
Anwesh43 <anweshthecool0@gmail.com>
2019-09-28 13:14:30 +0530

    adding fixes

--- squaresgrowlineview/src/main/java/com/anwesh/uiprojects/squaresgrowlineview/SquaresGrowLineView.kt
+++ squaresgrowlineview/src/main/java/com/anwesh/uiprojects/squaresgrowlineview/SquaresGrowLineView.kt
@@ -14,10 +14,10 @@ import android.view.View
 import android.view.MotionEvent
 
 val colors : Array<String> = arrayOf("#673AB7", "#E65100", "#4CAF50", "#f44336", "#0D47A1")
-val scGap : Float = 0.05f
+val scGap : Float = 0.01f
 val strokeFactor : Float = 90f
 val parts : Int = 6
-val delay : Long = 40
+val delay : Long = 10
 val backColor : Int = Color.parseColor("#BDBDBD")
 
 fun Int.inverse() : Float = 1f / this
@@ -27,12 +27,12 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Canvas.drawSquare(i : Int, gap : Float, sc : Float, paint : Paint) {
     save()
     translate(gap * 2 * i + 1.5f * gap, 0f)
-    drawRect(RectF(-gap / 2, 0f, gap / 2, gap * sc.divideScale(i + 1, parts)), paint)
+    drawRect(RectF(-gap / 2, 0f, gap / 2, gap * sc.divideScale(i + 2, parts)), paint)
     restore()
 }
 
 fun Canvas.drawLineGrowSquare(w : Float, sc : Float, paint : Paint) {
-    val gap : Float = w / 2 * (parts - 2) + 1
+    val gap : Float = w / (2 * (parts - 2) + 1)
     drawLine(0f, 0f, w * sc, 0f, paint)
     for (j in 0..(parts - 3)) {
         drawSquare(j, gap, sc, paint)
