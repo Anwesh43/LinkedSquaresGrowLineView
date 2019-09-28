ae73baa77e6120b6fc98f65c98c1af0e27655dea
Anwesh43 <anweshthecool0@gmail.com>
2019-09-28 12:07:22 +0530

    adding State class

--- squaresgrowlineview/src/main/java/com/anwesh/uiprojects/squaresgrowlineview/SquaresGrowLineView.kt
+++ squaresgrowlineview/src/main/java/com/anwesh/uiprojects/squaresgrowlineview/SquaresGrowLineView.kt
@@ -55,7 +55,7 @@ fun Canvas.drawSGLNode(i : Int, scale : Float, paint : Paint) {
 class SquaresGrowLineView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    
+
     override fun onDraw(canvas : Canvas) {
 
     }
@@ -67,5 +67,25 @@ class SquaresGrowLineView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scGap * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
