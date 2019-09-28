3216e5f9e76feb6bae4ccc17c6861cae94f12ca1
Anwesh43 <anweshthecool0@gmail.com>
2019-09-28 12:28:50 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquaresgrowlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquaresgrowlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsquaresgrowlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.squaresgrowlineview.SquaresGrowLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquaresGrowLineView.create(this)
     }
 }
