1efd5cca748cf85d6e5accd728118693b58a48ee
Anwesh43 <anweshthecool0@gmail.com>
2019-09-28 12:05:44 +0530

    adding SquaresGrowLineView class

--- squaresgrowlineview/src/main/java/com/anwesh/uiprojects/squaresgrowlineview/SquaresGrowLineView.kt
+++ squaresgrowlineview/src/main/java/com/anwesh/uiprojects/squaresgrowlineview/SquaresGrowLineView.kt
@@ -52,3 +52,20 @@ fun Canvas.drawSGLNode(i : Int, scale : Float, paint : Paint) {
     restore()
 }
 
+class SquaresGrowLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
