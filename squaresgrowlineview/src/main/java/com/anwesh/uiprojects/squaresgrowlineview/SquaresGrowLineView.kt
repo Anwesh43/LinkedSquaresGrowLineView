488467f824e26d1e506fc85c88a0203920c41dd0
Anwesh43 <anweshthecool0@gmail.com>
2019-09-28 12:12:59 +0530

    adding SGLNode class

--- squaresgrowlineview/src/main/java/com/anwesh/uiprojects/squaresgrowlineview/SquaresGrowLineView.kt
+++ squaresgrowlineview/src/main/java/com/anwesh/uiprojects/squaresgrowlineview/SquaresGrowLineView.kt
@@ -116,4 +116,46 @@ class SquaresGrowLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SGLNode(var i : Int, val state : State = State()) {
+
+        private var next : SGLNode? = null
+        private var prev : SGLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SGLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSGLNode(i, state.scale, paint)
+            prev?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SGLNode {
+            var curr : SGLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
