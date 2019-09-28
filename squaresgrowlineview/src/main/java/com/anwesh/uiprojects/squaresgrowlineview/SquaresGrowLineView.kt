34f405d155fc2fa690b76200e0e50c83e067d1bd
Anwesh43 <anweshthecool0@gmail.com>
2019-09-28 12:20:00 +0530

    adding SquareGrowLineContainer class

--- squaresgrowlineview/src/main/java/com/anwesh/uiprojects/squaresgrowlineview/SquaresGrowLineView.kt
+++ squaresgrowlineview/src/main/java/com/anwesh/uiprojects/squaresgrowlineview/SquaresGrowLineView.kt
@@ -155,7 +155,30 @@ class SquaresGrowLineView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class SquaresGrowLineContainer(var i : Int) {
+
+        private var curr : SGLNode = SGLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
