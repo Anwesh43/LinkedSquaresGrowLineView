12113c99f2bb490dfa32d4df82fe14fac77da9e0
Anwesh43 <anweshthecool0@gmail.com>
2019-09-28 11:46:33 +0530

    adding Int and Float extension methods

--- squaresgrowlineview/src/main/java/com/anwesh/uiprojects/squaresgrowlineview/SquaresGrowLineView.kt
+++ squaresgrowlineview/src/main/java/com/anwesh/uiprojects/squaresgrowlineview/SquaresGrowLineView.kt
@@ -20,3 +20,6 @@ val parts : Int = 6
 val delay : Long = 40
 val backColor : Int = Color.parseColor("#BDBDBD")
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
